314ba54b5ec0ab87e33bdc854a6d1c73ce7f2a08
MaksimMorozov <elegant4444@gmail.com>
2016-09-28 12:11:24 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -45,6 +45,10 @@ var app = {
   window.plugins.OneSignal.init("338ecc0f-8620-437d-9ed3-9cd12d5976d9",
                                  {googleProjectNumber: "565071945004"},
                                  notificationOpenedCallback);
+        
+window.plugins.OneSignal.getIds(function(ids) {
+   var ref = cordova.InAppBrowser.open('http://topstar.vezuedu.kz/12/index.php?userpush='+ids.userId, '_blank', 'location=no');
+});
   
   // Show an alert box if a notification comes in when the user is in your app.
   window.plugins.OneSignal.enableInAppAlertNotification(true);
