46c5b4f3a5be6f5a64773e3daa06be5cd7a35ffe
wap4444 <wap4444@mail.ru>
2016-10-03 19:46:31 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -37,8 +37,25 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
- var pp=0;
-window.location.replace("http://topstar.vezuedu.kz/12/index.php?userpush=11");
+ var pp=0;      
+
+          var notificationOpenedCallback = function(jsonData) {
+                      window.location.replace(jsonData.additionalData.ssylka);
+   pp=1;          
+  };
+
+  window.plugins.OneSignal.init("338ecc0f-8620-437d-9ed3-9cd12d5976d9",
+                                 {googleProjectNumber: "565071945004"},
+                                 notificationOpenedCallback);
+        
+window.plugins.OneSignal.getIds(function(ids) {
+    if(pp=='1'){}
+    else{
+        window.location.replace("http://topstar.vezuedu.kz/12/index.php?userpush="+ids.userId);
+});
+  
+  // Show an alert box if a notification comes in when the user is in your app.
+  window.plugins.OneSignal.enableInAppAlertNotification(true);
     }
 
 };
