3710b819a6b91eed686ac47f73a41f27c9c13e26
wap4444 <wap4444@mail.ru>
2016-10-05 21:45:27 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -38,50 +38,6 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-        
-        // Enable to debug issues.
-        // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-
-        var iosSettings = {};
-        iosSettings["kOSSettingsKeyAutoPrompt"] = true;
-        iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
-
-        window.plugins.OneSignal.startInit( "338ecc0f-8620-437d-9ed3-9cd12d5976d9", "")
-                                .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
-                                .handleNotificationOpened(didOpenRemoteNotificationCallBack)
-                                .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
-                                .iOSSettings(iosSettings)
-                                .endInit();
+     window.location.replace("http://topstar.vezuedu.kz/fr7");
     }
 };
-
-function didReceiveRemoteNotificationCallBack(jsonData) {
-        alert("Notification received:\n" + JSON.stringify(jsonData));
-        console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
-    }
-function didOpenRemoteNotificationCallBack (jsonData) {
-        alert("Notification opened:\n" + JSON.stringify(jsonData));
-        console.log('didOpenRemoteNotificationCallBack: ' + JSON.stringify(jsonData));   
-    }
-
-function sendTag() {
-    window.plugins.OneSignal.sendTag("PhoneGapKey", "PhoneGapValue");
-}
-function getIds() {
-    window.plugins.OneSignal.getIds(function(ids) {
-        document.getElementById("OneSignalUserId").innerHTML = "UserId: " + ids.userId;
-        document.getElementById("OneSignalPushToken").innerHTML = "PushToken: " + ids.pushToken;
-        console.log('getIds: ' + JSON.stringify(ids));
-    });
-}
-
-app.initialize();
-
