ae0ff983a41297cd4cdbba56a0815cefcc43af55
wap4444 <wap4444@mail.ru>
2016-10-05 17:26:26 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -37,7 +37,32 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-       window.location.replace("http://topstar.vezuedu.kz/fr7/index.php?userpush=11");
+         var pp=0;
+          var notificationOpenedCallback = function(jsonData) {
+   var ref = cordova.InAppBrowser.open(jsonData.additionalData.ssylka, '_blank', 'location=no');
+   pp=1;          
+  };
+
+  window.plugins.OneSignal.init("338ecc0f-8620-437d-9ed3-9cd12d5976d9",
+                                 {googleProjectNumber: "565071945004"},
+                                 notificationOpenedCallback);
+        
+window.plugins.OneSignal.getIds(function(ids) {
+    if(pp=='1'){}
+    else{
+               window.location.replace("http://topstar.vezuedu.kz/fr7/index.php?userpush="+ids.userId);
+    }
+});
+  
+  // Show an alert box if a notification comes in when the user is in your app.
+  window.plugins.OneSignal.enableInAppAlertNotification(true);
+        
+        
+        
+        
+        
+        
+
     }
 
 };
