142c7cc7341f7bee0b04e17c7afa5559a66fdf87
wap4444 <wap4444@mail.ru>
2016-10-14 13:38:52 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -53,11 +53,11 @@ var ref = cordova.InAppBrowser.open(jsonData.payload.additionalData.ssylka, '_bl
 function didOpenRemoteNotificationCallBack(jsonData) {
     rr=1;  
     
-    Для Andori
+   // Для Andori
 var newdata = JSON.parse ( jsonData.notification.payload.additionalData );
 var ref = cordova.InAppBrowser.open(newdata.ssylka , '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 
-    Для Iphone
+  //  Для Iphone
     //  var ref = cordova.InAppBrowser.open(jsonData.notification.payload.additionalData.ssylka, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
   
  }
