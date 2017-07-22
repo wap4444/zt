8041a764160671eed485c71aebf894bf3c52485d
wap4444 <wap4444@mail.ru>
2017-07-22 14:38:15 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -19,7 +19,26 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-var ref = cordova.InAppBrowser.open('http://mirada.kz/project_ksk/', '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+        //Настройка ПУШЕЙ ДЛЯ АЙФОНА
+        var iosSettings = {};
+        iosSettings["kOSSettingsKeyAutoPrompt"] = false;
+        iosSettings["kOSSettingsKeyInAppLaunchURL"] = true;
+        
+        //ПОДКЛЮЧЕНИЕ ПУШЕЙ
+           window.plugins.OneSignal
+          .startInit("82b9c889-5c3a-4526-abaf-271d6d269892")
+          .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
+          .handleNotificationOpened(didOpenRemoteNotificationCallBack)
+              .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
+          .iOSSettings(iosSettings)
+          .endInit();
+        
+        window.plugins.OneSignal.getIds(function(ids) {
+        ipush = ids.userId;
+            var ref = cordova.InAppBrowser.open('http://mirada.kz/project_ksk/index.html?push='+ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+        });
+        
+
     }
 };
 
